719ad337e4eb8136e00760c1469be867739362fa
Okugata Fahmi <otasrit@gmail.com>
2019-09-17 20:05:08 +0700

    Coba commit spl

---
+++ Spl.java
@@ -0,0 +1,12 @@
+public class Spl{
+    
+    public static void main(String[] args) {
+        System.out.println("");
+        //spl a = new spl();
+        //BacaSPL();
+    }
+
+    public static void BacaSPL(){
+        System.out.println("Ini hasilnya:");
+    }
+}
