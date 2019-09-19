b45e0a24e8a2197e5af6f714a19631b521f4abd7
taufiqhusada <taufiqhusdr@gmail.com>
2019-09-19 20:44:46 +0700

    .

--- Main.java
+++ Main.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 
 public class Main{
 	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args){
-		System.out.println("Hello World!");
+
+
+		// System.out.println("Hello World!");
 		Matriks mat = new Matriks();
 		mat.BacaMatriks();
 		mat.OutputMatriks();
-		Matriks mat1 = new Matriks();
-		mat1.MakeMatriks(mat.GetLastIdxBrs(), mat.GetLastIdxKol(), mat.M);
+		System.out.println(mat.DeterminanOBE());
 
-		mat1.GaussJordan();
-		mat1.OutputMatriks();
+		
+		// Matriks mat1 = new Matriks();
+		// mat1.MakeMatriks(mat.GetLastIdxBrs(), mat.GetLastIdxKol(), mat.M);
 
-		double []hasil = mat.Cramer();
-		for (int i = 1; i<=mat.GetLastIdxKol()-1; ++i){
-			System.out.println(hasil[i]);
-		}
+		
+		// Matriks matInverse2 = mat1.MatriksInverseOBE();
+		// matInverse2.OutputMatriks();
+
+		// System.out.println("==========");
+		// Matriks matInverse = mat.MatriksInverse();
+		// matInverse.OutputMatriks();
+		
+
+		// double []hasil = mat.Cramer();
+		// for (int i = 1; i<=mat.GetLastIdxKol()-1; ++i){
+		// 	System.out.println(hasil[i]);
+		// }
 		/*	Untuk mendebug Spl
 		Spl data = new Spl();
 		data.BacaSPL();
